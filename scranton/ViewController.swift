7faa837ffeb236a9dad54e51ce9dbdc4a5f03021
Asherlee Levy <asherleelevy@asherlees-macbook-pro.local>
2016-05-10 12:46:21 -0400

    Initial Commit

---
+++ scranton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scranton
+//
+//  Created by Asherlee Levy on 5/10/16.
+//  Copyright © 2016 Asherlee Levy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
